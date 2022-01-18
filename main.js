e01e7158b813d092fa76fee04ba9cbaba295203f
HayaiAlex <alexadams2084@gmail.com>
2022-01-18 15:25:53 +0000

    Added all answers area

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ async function startButtonClicked() {
 
     // show question container and set starting time
     showQuestionContainer();
+    let questionData = {};
     let startTime = Date.now();
     console.log(startTime);
 
@@ -18,8 +19,10 @@ async function startButtonClicked() {
     let questionQuantity = 20
     let correctAnswers = 0;
     for (let questionNum=1; questionNum <= questionQuantity; questionNum++) {
-        const result = await askQuestion(questionNum);
-        if (result === true) {
+        const results = await askQuestion(questionNum);
+        let key = "question"+questionNum;
+        questionData[key] = results;
+        if (results.correct === true) {
             correctAnswers++;
         }
     }
@@ -31,7 +34,8 @@ async function startButtonClicked() {
     // show results information
     let incorrectAnswers = questionQuantity - correctAnswers;
     let timeElapsed = endTime - startTime;
-    showResults(incorrectAnswers,timeElapsed);
+    showResults(incorrectAnswers,timeElapsed,questionData);
+    console.log(questionData);
 
 }
 
@@ -46,17 +50,70 @@ function hideQuestionContainer() {
     questionContainer.remove();
 }
 
-function showResults(incorrectAnswers,milliseconds) {
+function showResults(incorrectAnswers,milliseconds,questionData) {
+    const resultsContainer = document.getElementById("results-container");
     const resultsTemplate = document.getElementById("results-template");
     const resultsClone = resultsTemplate.content.cloneNode(true);
-    document.body.querySelector("main").append(resultsClone);
+    resultsContainer.append(resultsClone);
 
     const scoreText = document.getElementById("score").querySelector("p");
     scoreText.innerText = incorrectAnswers;
 
-    const timeText = document.getElementById("time").querySelector("p");
+    const averageTime = document.getElementById("time").getElementsByTagName("p")[0];
+    let averageT = 0;
+    for (let question in questionData) {
+        averageT += questionData[question].timeTaken;
+    }
+    averageT /= Object.keys(questionData).length;
+    averageT = formatTime(averageT);
+    averageTime.innerText = `Average: ${averageT}`
+
+    const timeTotalText = document.getElementById("time").getElementsByTagName("p")[1];
     let formattedTime = formatTime(milliseconds);
-    timeText.innerText = `Completed in: ${formattedTime}`;
+    timeTotalText.innerText = `Completed in: ${formattedTime}`;
+
+    const answerButton = document.getElementById("show-answers");
+    answerButton.addEventListener("click",() => {
+
+        showAnswers(questionData);
+    });
+}
+
+function showAnswers(questions) {
+    console.log("show answers")
+
+    const answers = document.getElementById("answers-container");
+    const answerTemplate = document.getElementById("answer-template");
+
+    const totalQuestions = Object.keys(questions).length;
+    for (let i=1;i<=totalQuestions;i++) {
+        const answer = answerTemplate.content.cloneNode(true);
+        answers.append(answer);
+
+        let answerBox = document.getElementsByClassName("answer");
+        answerBox = answerBox[answerBox.length - 1];
+        console.log(answerBox);
+        num = "question"+i;
+
+        answerBox.querySelector("h4").innerText = `Question ${questions[num].questionNumber}`;
+        let question = questions[num].question.substring(8,questions[num].question.length-1);
+        question += ` = ${questions[num].givenAnswer}`;
+        answerBox.getElementsByClassName("question")[0].innerText = question;
+        
+        if (questions[num].correct) {
+            answerBox.getElementsByClassName("cross")[0].remove();
+            answerBox.getElementsByClassName("description")[0].innerText = "Good job!";
+        } else {
+            answerBox.getElementsByClassName("check")[0].remove();
+            answerBox.getElementsByClassName("description")[0].innerText = `Answer: ${questions[num].correctAnswer}`;
+        }
+        
+        const time = formatTime(questions[num].timeTaken);
+        answerBox.getElementsByClassName("time")[0].innerText = `Time: ${time}`;
+
+    }
+        
+
 }
 
 function formatTime(milliseconds) {
@@ -66,11 +123,18 @@ function formatTime(milliseconds) {
         return i;
     }
 
+    function addMillisecondZeroes(i) {
+        if (i < 10) {i = "00" + i}
+        else if (i < 100) {i = "0" + i}
+        return i;
+    }
+
     let d = new Date();
     d.setTime(milliseconds);
     let m = addZero(d.getMinutes());
     let s = addZero(d.getSeconds());
-    return m + ":" + s;
+    let ms = addMillisecondZeroes(d.getMilliseconds());
+    return `${m}:${s}:${ms}`;
 }
 
 function askQuestion(questionNum) {
@@ -102,6 +166,7 @@ function askQuestion(questionNum) {
     question.innerText = `What is ${num1} ${operator} ${num2}?`;
 
     console.log(`Set question: What is ${num1} ${operator} ${num2}?`);
+    const startTime = Date.now();
 
     // Submit pressed, check answer
     return answerPromise = new Promise( resolve => {
@@ -114,13 +179,23 @@ function askQuestion(questionNum) {
             if (event.type == "click" || event.type == "keydown" && event.key == "Enter") {
                 // return if the answer is correct
                 const result = checkAnswer(num1,num2,operator);
+                const timeElapsed = Date.now() - startTime;
 
                 // remove event listeners to stop duplicate events existing simultaneiously
                 submitButton.removeEventListener("click",submittedAnswer);
                 textBox.removeEventListener("keydown",submittedAnswer);
 
-                // finally return the result
-                resolve(result);
+
+                // finally return the results
+                let results = {
+                    questionNumber: questionNum,
+                    question: question.innerText,
+                    timeTaken: timeElapsed,
+                    correctAnswer: result.answer,
+                    givenAnswer: result.givenAnswer,
+                    correct: result.correct,
+                }
+                resolve(results);
             }
         }
 
@@ -136,16 +211,16 @@ function askQuestion(questionNum) {
             switch (operator) {
                 case "+":
                     console.log(num1+num2,userAnswer,num1+num2 === userAnswer)
-                    return num1+num2 === userAnswer;
+                    return {answer: num1+num2, givenAnswer: userAnswer, correct: num1+num2 === userAnswer};
                 case "-":
                     console.log(num1-num2,userAnswer,num1-num2 === userAnswer)
-                    return num1-num2 === userAnswer;
+                    return {answer: num1-num2, givenAnswer: userAnswer, correct: num1-num2 === userAnswer};
                 case "x":
                     console.log(num1*num2,userAnswer,num1*num2 === userAnswer)
-                    return num1*num2 === userAnswer;
+                    return {answer: num1*num2, givenAnswer: userAnswer, correct: num1*num2 === userAnswer};
                 case "÷":
                     console.log(num1/num2,userAnswer,num1/num2 === userAnswer)
-                    return num1/num2 === userAnswer;
+                    return {answer: num1/num2, givenAnswer: userAnswer, correct: num1/num2 === userAnswer};
             }
         }
     });
